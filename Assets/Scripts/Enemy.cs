1c657ed1cc0c372ef5cbfdb9710b6a8cc48935be
gorilaSam <48575634+gorilaSam@users.noreply.github.com>
2019-12-03 00:41:16 -0600

    Working on enemy bullet sprites/objects

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -27,10 +27,10 @@ public class Enemy : MonoBehaviour
     void Shoot()
     {
         //bullet.bulletForce = bulletForce;
-        GameObject bulletObj = Instantiate(bullet, firePoint.position, Quaternion.identity);
+        GameObject bulletObj = Instantiate(bullet, firePoint.position, bullet.transform.rotation);
         Rigidbody2D rb = bulletObj.GetComponent<Rigidbody2D>();
         
-        rb.velocity = new Vector2(rb.velocity.x, -bulletForce + 2);
+        //rb.velocity = new Vector2(rb.velocity.x, -bulletForce);
         Destroy(bulletObj, 3);
     }
 
--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -10,6 +10,7 @@ public class EnemyBullet : MonoBehaviour
     Rigidbody2D rb;
     Vector2 trajectory;
     float bulletForce = 15f;
+    Quaternion newQuaternion;
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -28,11 +29,12 @@ public class EnemyBullet : MonoBehaviour
         player = GameObject.FindObjectOfType<Player>();
         trajectory = (player.transform.position - transform.position).normalized * bulletForce;
         rb.velocity = new Vector2(trajectory.x, trajectory.y);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, player.transform.position, (bulletForce * Time.deltaTime));
     }
 }
